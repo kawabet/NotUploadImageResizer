11adf9aa30d005d167b28cea7e7bd26e09bbd76c
Taisuke Kawabe <tkwb54@gmail.com>
2019-08-18 22:27:45 +0900

    メソッド分離、重複コードの整理

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  *
  * @param {*} src
@@ -5,116 +7,110 @@
  * @param {*} name
  */
 export function base64ToFile(src, resize, name) {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
   const image = new Image();
-  // これでMIMEタイプが取れてると思うけど若干不安...
-  const type = src.substring(0, src.indexOf(";")).replace("data:", "");
   image.src = src;
   return new Promise((resolve, reject) => {
     image.onload = () => {
-      // リサイズ後のサイズを計算する
-      let ratio, width, height;
-      if (resize && resize > 0) {
-        if (image.width > image.height) {
-          ratio = image.height / image.width;
-          width = resize;
-          height = resize * ratio;
-        } else {
-          ratio = image.width / image.height;
-          width = resize * ratio;
-          height = resize;
-        }
-      } else {
-        width = image.width;
-        height = image.height;
-      }
-
-      // canvasをリサイズ後のサイズで作る
-      canvas.height = height;
-      canvas.width = width;
-      context.drawImage(image, 0, 0, width, height);
-
-      // canvasからbase64画像データを取得
-      const base64 = canvas.toDataURL(type);
-      // base64からBlobデータを作成
-      let barr, bin, i, len;
-      bin = atob(base64.split("base64,")[1]);
-      len = bin.length;
-      barr = new Uint8Array(len);
-
-      i = 0;
-      while (i < len) {
-        barr[i] = bin.charCodeAt(i);
-        i++;
-      }
-
+      // これでMIMEタイプが取れてると思うけど若干不安...
+      const type = src.substring(0, src.indexOf(";")).replace("data:", "");
+      // リサイズしてbase64化
+      let base64 = resizeImage(image, type, resize);
       // 引数にnameが設定されていない場合
-      if (!name) name = `${Date.now()}.${type.replace("image/", "")}`;
+      if (!name) {
+        name = `${Date.now()}.${type.replace("image/", "")}`;
+      }
       // fileオブジェクトを作成
-      const file = new File([barr], name, { type: type });
+      let file = createFile(base64, name, type)
       resolve(file);
     };
     image.onerror = e => reject(e);
   });
 }
 
-//**
-/*
+/**
+ *
  * @param {*} src
  * @param {*} resize
  */
 export function pathToFile(src, resize) {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
   const image = new Image();
-
-  const name_ext = src.match(".+/(.+?)([?#;].*)?$")[1];
-  let ext = name_ext.substring(name_ext.lastIndexOf(".") + 1, name_ext.length);
-  if (ext == "jpg") ext = "jpeg"; // なんか image/jpg　でbase64化すると image/png　になってしまう。
-  const type = `images/${ext}`;
   image.src = src;
-
   return new Promise((resolve, reject) => {
     image.onload = () => {
-      let ratio, width, height;
-      if (resize && resize > 0) {
-        if (image.width > image.height) {
-          ratio = image.height / image.width;
-          width = resize;
-          height = resize * ratio;
-        } else {
-          ratio = image.width / image.height;
-          width = resize * ratio;
-          height = resize;
-        }
-      } else {
-        width = image.width;
-        height = image.height;
+      // nameとtypeを取得
+      const name_ext = src.match(".+/(.+?)([?#;].*)?$")[1];
+      let ext = name_ext.substring(
+        name_ext.lastIndexOf(".") + 1,
+        name_ext.length
+      );
+      if (ext == "jpg") {
+        ext = "jpeg"; // なんか image/jpg　でbase64化すると image/png　になってしまう。
       }
-      canvas.height = height;
-      canvas.width = width;
-      context.drawImage(image, 0, 0, width, height);
-
-      const base64 = canvas.toDataURL(type);
-      // base64からBlobデータを作成
-      let barr, bin, i, len;
-      bin = atob(base64.split("base64,")[1]);
-      len = bin.length;
-      barr = new Uint8Array(len);
-
-      i = 0;
-      while (i < len) {
-        barr[i] = bin.charCodeAt(i);
-        i++;
-      }
-
+      const type = `images/${ext}`;
+      // リサイズしてbase64化
+      let base64 = resizeImage(image, type, resize);
       // fileオブジェクトを作成
-      const file = new File([barr], name_ext, { type: type });
+      let file = createFile(base64, name, type)
       resolve(file);
     };
     image.onerror = e => reject(e);
   });
+}
+
+/**
+ * 指定されたimageをリサイズしてbase64として返します
+ * @param {*} image
+ * @param {*} type
+ * @param {*} resize
+ */
+export function resizeImage(image, type, resize) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+
+  // リサイズ後のサイズを計算する
+  let ratio, width, height;
+  if (resize && resize > 0) {
+    if (image.width > image.height) {
+      ratio = image.height / image.width;
+      width = resize;
+      height = resize * ratio;
+    } else {
+      ratio = image.width / image.height;
+      width = resize * ratio;
+      height = resize;
+    }
+  } else {
+    width = image.width;
+    height = image.height;
+  }
+  // canvasをリサイズ後のサイズで作る
+  canvas.height = height;
+  canvas.width = width;
+  context.drawImage(image, 0, 0, width, height);
+
+  // canvasからbase64画像データを取得
+  return canvas.toDataURL(type);
+}
+
+/**
+ *
+ * @param {*} base64_image
+ * @param {*} filename
+ * @param {*} type
+ */
+function createFile(base64, filename, type) {
+  // base64からBlobデータを作成
+  let bin = atob(base64.split("base64,")[1]);
+  let len = bin.length;
+  let barr = new Uint8Array(len);
+  let i = 0;
+  while (i < len) {
+    barr[i] = bin.charCodeAt(i);
+    i++;
+  }
+  // Blobをファイル化
+  let file = new File([barr], filename, { type: type });
+  return file;
 }
 
 /**
